3eb984aacb4843340a098b9de25d66e08b5db986
ZeendaBean24 <liuzeen999@gmail.com>
2023-12-16 12:15:14 -0500

    Add template cpp file

--- template.cpp
+++ template.cpp
@@ -1,8 +1,13 @@
 #include <bits/stdc++.h>
-#include <cmath>
 
 using namespace std;
 
+#define endl "\n";
+#define fastio ios_base::sync_with_stdio(false); cin.tie(nullptr);
+
+typedef long long ll;
+
 int main() {
-    
- } 
+
+    return 0;
+}
