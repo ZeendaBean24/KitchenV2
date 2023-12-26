f8e1faf8c32a0b8c777392dbb9729cbd2a6789c4
ZeendaBean24 <liuzeen999@gmail.com>
2023-12-26 12:46:39 -0500

    Update _template.cpp, more macros and typedefs

--- _template.cpp
+++ _template.cpp
@@ -2,10 +2,12 @@
 
 using namespace std;
 
-#define endl "\n";
-#define fastio ios_base::sync_with_stdio(false); cin.tie(nullptr);
+typedef long long ll; typedef long double ld;
+typedef vector<int> vi; typedef vector<string> vs; typedef vector<char> vc;
 
-typedef long long ll;
+#define fastio ios_base::sync_with_stdio(false); cin.tie(nullptr);
+#define REP(i,a,b) for (int i = a; i <= b; i++);
+#define SQ(a) (a)*(a);
 
 int main() {
 
