1923b71b3fd36439074eaed0c916aa364fdbfd41
ZeendaBean24 <liuzeen999@gmail.com>
2023-12-28 19:17:51 -0500

    Edit _template.cpp by adding typedef for pairs

--- _template.cpp
+++ _template.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 typedef long long ll; typedef long double ld;
 typedef vector<int> vi; typedef vector<string> vs; typedef vector<char> vc;
+typedef pair<int, int> pii; typedef pair<long long, long long> pll;
 
 #define fastio ios_base::sync_with_stdio(false); cin.tie(nullptr)
 #define REP(a,b) for (int i = a; i < b; i++)
