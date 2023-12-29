a47bc03d2c6544f4840b9535d0afb973ac4f10c4
ZeendaBean24 <liuzeen999@gmail.com>
2023-12-28 19:16:15 -0500

    Optimize Burglar_and_Matches.cpp for AC (TLE before)

--- JuniorProgrammingSheet/Div-B/Burglar_and_Matches.cpp
+++ JuniorProgrammingSheet/Div-B/Burglar_and_Matches.cpp
@@ -5,33 +5,36 @@ using namespace std;
 typedef long long ll; typedef long double ld;
 typedef vector<int> vi; typedef vector<string> vs; typedef vector<char> vc;
 
-#define fastio ios_base::sync_with_stdio(false); cin.tie(nullptr);
+#define fastio ios_base::sync_with_stdio(false); cin.tie(nullptr)
 #define REP(a,b) for (int i = a; i < b; i++)
 #define SQ(a) (a)*(a);
 
 int main() {
-
-	int n, m; cin >> n >> m;
-	vector<vi> containers;
+	fastio;
+	ll n, m; cin >> n >> m;
+	vector<pair<int, int>> c(m);
 
 	REP(0, m) {
-		int x, y; cin >> x >> y;
-		containers.push_back({x, y});
+		int fir, sec; cin >> sec >> fir;
+		c[i].first = fir, c[i].second = sec;
 	}
 
-	sort(containers.begin(), containers.end(), [](const vector<int> &a, const vector<int> &b) {
-        return a[1] > b[1];
-    });
+	sort(c.begin(), c.end());
+	reverse(c.begin(), c.end());
 
-	int currentIndex = 0;
-	int count = 0;
-	while (n != 0 && containers[m-1][0] != 0) {
-		if (containers[currentIndex][0] == 0) {
-			currentIndex++;
-		}
-		count += containers[currentIndex][1];
-		containers[currentIndex][0]--;
-		n--;
+	int id = 0;
+	int rem = n; 
+	ll count = 0;
+
+	while (id != m) {
+		if (rem >= c[id].second) {
+			count += c[id].first * c[id].second;
+			rem -= c[id].second;
+			id++;
+		} else {
+			count += rem * c[id].first;
+			break;
+		} 
 	}
 
 	cout << count;
--- _template.cpp
+++ _template.cpp
@@ -5,7 +5,7 @@ using namespace std;
 typedef long long ll; typedef long double ld;
 typedef vector<int> vi; typedef vector<string> vs; typedef vector<char> vc;
 
-#define fastio ios_base::sync_with_stdio(false); cin.tie(nullptr);
+#define fastio ios_base::sync_with_stdio(false); cin.tie(nullptr)
 #define REP(a,b) for (int i = a; i < b; i++)
 #define SQ(a) (a)*(a);
 
