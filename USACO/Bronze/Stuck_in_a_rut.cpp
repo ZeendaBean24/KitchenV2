9b65e690de0989cd3c576e84d40fb10b213cfa6f
ZeendaBean24 <liuzeen999@gmail.com>
2024-01-25 14:26:26 -0500

    Update Stuck_in_a_rut.cpp

--- USACO/Bronze/Stuck_in_a_rut.cpp
+++ USACO/Bronze/Stuck_in_a_rut.cpp
@@ -11,26 +11,35 @@ typedef pair<int, int> pii; typedef pair<long long, long long> pll;
 #define SQ(a) (a)*(a)
 
 int Infinity = 1000000001;
+
 struct Cow {
-    char direction;
-    int x, y, grassEaten;
-    bool stopped;
+    int time_stopped;
+    int x, y;
+    char dir;
 };
 
-int timeToHitRut(Cow i, Cow j) {
-    if (i.direction == j.direction) return -1;
-    if (i.direction == 'N' && j.y >= i.y && i.x >= j.x && j.y - i.y >= i.x - j.x)
-        if (j.y - i.y == i.x - j.x) {
-            return -1;
-        } 
-        return i.x - j.y;
-    if (i.direction == 'E' && j.x >= i.x && i.y >= j.y && j.x - i.x >= i.y - j.y) {
-        if (j.x - i.x == i.y - j.y) {
-            return -1;
-        } 
-        return i.y - j.y;
+int n;
+Cow cows[50];
+
+int when_hits(int i, int j, int currentTime) {
+    Cow I = cows[i]; 
+    Cow J = cows[j];
+    if (I.dir == J.dir) {
+        return Infinity;
     }
-    return -1;
+    if (I.dir == 'E') { // I moves North, J moves East
+        swap(I.x, I.y);
+        swap(J.x, J.y);
+    }
+    if (I.y >= J.y) {
+        return Infinity;
+    }
+    if (J.time_stopped == Infinity) {
+        
+    }
+
+
+    return Infinity;
 }
 
 int main() {
@@ -38,19 +47,5 @@ int main() {
     // freopen("problemname.in", "r", stdin);
     // freopen("problemname.out", "w", stdout);
 
-    int n; cin >> n;
-    vector<Cow> cows(n);
 
-    REP(i,0,n) {
-        int x, y;
-        char dir; 
-        cin >> cows[i].direction >> cows[i].x >> cows[i].y;
-        cows[i].grassEaten = 0; cows[i].stopped = false;
-    }
-
-    for (Cow cow : cows) {
-        for (Cow c : cows) {
-            cout << timeToHitRut(cow, c) << "    " << "Cow 1: " << cow.x << " " << cow.y << " " << cow.direction << "   Cow 2: " << c.x << " " << c.y << " " << c.direction << endl;
-        }
-    }
 }
