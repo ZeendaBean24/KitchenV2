003fa120de857640813d56cd483296344f029c3a
ZeendaBean24 <liuzeen999@gmail.com>
2024-09-07 11:39:30 -0400

    Move Breed_Counting.cpp from Bronze to Silver

---
+++ USACO/Bronze/Subsequences_Summer_to_Sevens.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll; typedef long double ld;
+typedef vector<int> vi; typedef vector<double> vd; typedef vector<string> vs; typedef vector<char> vc; typedef vector<long long> vll;
+typedef pair<int, int> pii; typedef pair<long long, long long> pll;
+
+#define fastio ios_base::sync_with_stdio(false); cin.tie(nullptr)
+#define REP(i,a,b) for (int i = a; i < b; i++)
+#define SQ(a) (a)*(a)
+
+int main() {
+    fastio;
+    // freopen("problemname.in", "r", stdin);
+    // freopen("problemname.out", "w", stdout);
+
+    
+
+    return 0;
+}
