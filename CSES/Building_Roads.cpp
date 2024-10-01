00290cbbd5668bc9c7fc27c65fcbe306e72ba7ab
ZeendaBean24 <liuzeen999@gmail.com>
2024-09-30 21:01:17 -0400

    Update _template.cpp

--- CSES/Building_Roads.cpp
+++ CSES/Building_Roads.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 typedef long long ll; typedef long double ld;
-typedef vector<int> vi; typedef vector<double> vd; typedef vector<string> vs; typedef vector<char> vc; typedef vector<long long> vll; typedef vector<bool> vb;
+typedef vector<int> vi; typedef vector<double> vd; typedef vector<string> vs; typedef vector<char> vc; typedef vector<long long> vll;
 typedef pair<int, int> pii; typedef pair<long long, long long> pll;
 
 #define fastio ios_base::sync_with_stdio(false); cin.tie(nullptr)
@@ -14,6 +14,15 @@ int main() {
     fastio;
     // freopen("problemname.in", "r", stdin);
     // freopen("problemname.out", "w", stdout);
+
+    int n, m; cin >> n >> m;
+
+    vector<vi> adj(n+1);
+    REP(i,0,m) {
+    	int a, b; cin >> a >> b;
+    	adj[a].push_back(b);
+    	adj[b].push_back(a);
+    }
 
     return 0;
 }
