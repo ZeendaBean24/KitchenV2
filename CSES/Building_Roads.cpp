4eaccd16c514aeaa537a3ef6aed82567529d04be
ZeendaBean24 <liuzeen999@gmail.com>
2024-10-01 00:02:25 -0400

    Add Building_Roads.cpp

--- CSES/Building_Roads.cpp
+++ CSES/Building_Roads.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 typedef long long ll; typedef long double ld;
-typedef vector<int> vi; typedef vector<double> vd; typedef vector<string> vs; typedef vector<char> vc; typedef vector<long long> vll;
+typedef vector<int> vi; typedef vector<double> vd; typedef vector<string> vs; typedef vector<char> vc; typedef vector<long long> vll; typedef vector<bool> vb;
 typedef pair<int, int> pii; typedef pair<long long, long long> pll;
 
 #define fastio ios_base::sync_with_stdio(false); cin.tie(nullptr)
@@ -22,6 +22,34 @@ int main() {
     	int a, b; cin >> a >> b;
     	adj[a].push_back(b);
     	adj[b].push_back(a);
+    }
+
+    vb visited(n+1, false);
+    vi reps;
+
+    REP(i,1,n+1) {
+    	if (!visited[i]) {
+    		reps.push_back(i);
+    		queue<int> q;
+    		q.push(i);
+    		visited[i] = true;
+    		while (!q.empty()) {
+    			int city = q.front();
+    			q.pop();
+    			for (int neighbour : adj[city]) {
+    				if (!visited[neighbour]) {
+    					visited[neighbour] = true;
+    					q.push(neighbour);
+    				}
+    			}
+    		}
+    	}
+    }
+
+    int numRoads = reps.size() - 1;
+    cout << numRoads << endl;
+    REP(i,1,numRoads+1) {
+    	cout << reps[i-1] << " " << reps[i] << endl;
     }
 
     return 0;
