084614a5e00522a9b86239b6b85b93466a29596a
ZeendaBean24 <liuzeen999@gmail.com>
2024-11-22 21:10:06 -0500

    Add Moocast.cpp

--- USACO/Silver/Moocast.cpp
+++ USACO/Silver/Moocast.cpp
@@ -10,10 +10,58 @@ typedef pair<int, int> pii; typedef pair<long long, long long> pll;
 #define REP(i,a,b) for (int i = a; i < b; i++)
 #define SQ(a) (a)*(a)
 
+int dfs(int node, vb &visited, vector<vi> adj) {
+    int count = 1;
+    visited[node] = true;
+    for (auto neighbour : adj[node]) {
+        if (!visited[neighbour]) {
+            count += dfs(neighbour, visited, adj);
+        }
+    }
+    return count;
+}
+
 int main() {
     fastio;
-    // freopen("problemname.in", "r", stdin);
-    // freopen("problemname.out", "w", stdout);
+    freopen("moocast.in", "r", stdin);
+    freopen("moocast.out", "w", stdout);
+
+    int n; cin >> n;
+
+    vector<vi> cows;
+    REP(i,0,n) {
+        int x, y, p; cin >> x >> y >> p;
+        cows.push_back({x, y, p});
+    }
+
+    vector<vi> adj(n);
+    REP(i,0,n) {
+        REP(j,i+1,n) {
+            int x1, x2, y1, y2, p1, p2;
+            x1 = cows[i][0]; x2 = cows[j][0]; y1 = cows[i][1]; y2 = cows[j][1]; p1 = cows[i][2]; p2 = cows[j][2];
+            if (SQ(x2 - x1) + SQ(y2 - y1) <= SQ(p1)) {
+                adj[i].push_back(j);
+            } 
+            if (SQ(x1 - x2) + SQ(y1 - y2) <= SQ(p2)) {
+                adj[j].push_back(i);
+            }
+        }
+    }
+
+    // for (vi v : adj) {
+    //     for (int i : v) {
+    //         cout << i << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+    int maximum = 0;
+    REP(i,0,n) {
+        vb visited(n, false);
+        maximum = max(maximum, dfs(i, visited, adj));
+    }
+
+    cout << maximum;
 
     return 0;
 }
