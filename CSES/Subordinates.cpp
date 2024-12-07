6532f057d69275ebea03d27646ca7022906a59d2
ZeendaBean24 <liuzeen999@gmail.com>
2024-12-07 11:22:20 -0500

    add Subordinates.cpp

--- CSES/Subordinates.cpp
+++ CSES/Subordinates.cpp
@@ -10,36 +10,36 @@ typedef pair<int, int> pii; typedef pair<long long, long long> pll;
 #define REP(i,a,b) for (int i = a; i < b; i++)
 #define SQ(a) (a)*(a)
 
-int MAXSIZE = 1000;
-// vector<vi> grid(MAXSIZE, vi(MAXSIZE));
-vector<vi> grid(6, vi(6));
-// vector<vb> visited(MAXSIZE, vb(MAXSIZE, false));
-vector<vb> visited(6, vb(6, false));
+const int SZ = 2e5;
+vi countArr(SZ+1, 0);
+vector<vi> adj(SZ+1);
+
+void dfs(int s, int e) {
+	countArr[s] = 1;
+	for (auto u : adj[s]) {
+		if (u == e) continue;
+		dfs(u, s);
+		countArr[s] += countArr[u];
+	}
+}
 
 int main() {
     fastio;
     // freopen("problemname.in", "r", stdin);
     // freopen("problemname.out", "w", stdout);
+
     int n; cin >> n;
-    REP(i,0,n) {
-        string chars; cin >> chars;
-        REP(j,0,n) {
-            if (chars[j] == '#') {
-                grid[i][j] = 1;
-            } else {
-                grid[i][j] = 0;
-            }
-        }
+    REP(i,2,n+1) {
+    	int e; cin >> e;
+    	adj[e].push_back(i);
     }
 
-    // for (vi row : grid) {
-    //     for (int i : row) {
-    //         cout << i << " ";
-    //     }
-    //     cout << endl;
-    // }
+    dfs(1, 0);
 
-    pii maximumArea = {0, 0};
+    REP(i,1,n+1) {
+    	cout << countArr[i]-1 << " ";
+    }
+
 
     return 0;
 }
